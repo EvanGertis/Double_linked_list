88e47223e0a1ea99f3f7ac40e1bbf49834a0773d
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2018-12-06 06:32:41 -0500

    Initial commit

---
+++ demo_1.c
@@ -0,0 +1,168 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct node
+{
+	struct node *prev;
+	int info;
+	struct node *next;
+};
+
+struct node *createList(struct node *start);
+void displayList(struct node *start);
+struct node *insertInEmptyList(struct node *start, int data);
+struct node *insertInBeginning(struct node *start, int data);
+void insertAtEnd(struct node *start, int data);
+void insertAfter(struct node *start, int data, int x);
+struct node *insertBefore(struct node *start, int data);
+struct node *deleteNode(struct node *start, int data);
+struct node *reverseList(struct node *start);
+
+
+int main() 
+{
+	int choice, data, x;
+	struct node *start = NULL;
+	
+	start = createList(start);
+	
+	while(1){
+		printf("\n");
+		printf("1. Display List \n");
+		printf("2. Insert in empty list \n");
+		printf("3. Insert a node in beginning of the list \n");
+		printf("4. Insert a node at the end of the list \n");
+		printf("5. Insert a node after a specified node \n");
+		printf("6. Insert a node before a specified node \n");
+		printf("7. Delete a node \n");
+		printf("8. Reverse the list \n");
+		printf("9. Quit \n");
+		printf("enter your choice : ");
+		scanf("%d", &choice);
+		
+		if(choice = 9){
+			break;
+		}
+		
+		switch(choice){
+			case 1:
+				displayList(start);
+				break;
+			case 2: 
+				printf("Enter the element to be inserted : ");
+				scanf("%d", &data);
+				start = insertInEmptyList(start, data);
+				break;
+			case 3:
+				printf("Enter the element to be inserted : ");
+				scanf("%d", &data);
+				start = insertInBeginning(start, data);
+				break;
+			case 4: 
+				printf("Enter the element to be inserted : ");
+				scanf("%d", &data);
+				insertAtEnd(start, data);
+				break;
+			case 5:
+				printf("Enter the node which you would like to insert data after : ");
+				scanf("%d", &x);
+				printf("Enter the element that you would like to be inserted : ");
+				insertAfter(start, data, x);
+				break;
+			case 6:
+				printf("Enter the node which you would like to insert data before : ");
+				scanf("%d", &x);
+				printf("Enter the element that you would like to be inserted : ");
+				start = insertBefore(start, data);
+				break;
+			case 7:
+				printf("Enter the element of the node that you would like to delete : ");
+				start = deleteNode(start, data);
+				break;
+			case 8: 
+				start = reverseList(start);
+				break;
+			default:
+				printf("wrong choice");
+				break;
+		}/*end switch*/
+		
+	}/*end while*/
+	
+	return 0;
+}/*end main*/
+
+struct node *createList(struct node *start){
+	int i, n, data;
+	struct node *temp;
+	temp = (struct node *)malloc(sizeof(struct node));
+	
+	printf("Enter the number of nodes : ");
+	scanf("%d", n);
+	
+	if(n == 1){
+		printf("Enter the data for node %d : ", &n);
+		scanf("%d", &data);
+		insertInEmptyList(start, data);
+		return start;
+	}
+	
+	for(i = 1; i < n; i++){
+		printf("Enter the data for node %d : ", &n);
+		scanf("%d", &data);
+		
+	}
+	
+	return start;
+}/* end createList */
+
+void displayList(struct node *start){
+	struct node *p;
+	if(start == NULL){
+		printf("The list is empty");
+		return;
+	}
+	
+	p = start;
+	printf("List is :\n");
+	while(p != NULL){
+		printf("%d", p->info);
+		p = p->next;
+	}
+	printf("\n");
+	
+}/* end displayList */
+
+struct node *insertInEmptyList(struct node *start, int data){
+	
+	return start;
+}/* end insertInEmptyList */
+
+struct node *insertInBeginning(struct node *start, int data){
+	
+	return start;
+}/* end insertInBeginning */
+
+void insertAtEnd(struct node *start, int data){
+	
+}/* end insertAtEnd */
+
+void insertAfter(struct node *start, int data, int x){
+	
+}/* end insertAfter */
+
+struct node *insertBefore(struct node *start, int data){
+	
+	return start;
+}/* end insertBefore */
+
+struct node *deleteNode(struct node *start, int data){
+	
+	return start;
+}/* end deleteNode*/
+
+struct node *reverseList(struct node *start){
+	
+	return start;
+}/* end reverseList */
+
